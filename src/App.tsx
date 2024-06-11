9c727455ba0b251cf07ed7bd49c579ac1f5bb9f2
Pub <96320073+b3Rhunter@users.noreply.github.com>
2024-06-11 12:50:00 -0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
-import PWABadge from './PWABadge.tsx'
+import { CoinbaseWalletSDK } from '@coinbase/wallet-sdk';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [connected, setConnected] = useState(false)
+
+  const connect = async () => {
+    try {
+      const sdk = new CoinbaseWalletSDK({
+        appName: 'My Dapp',
+        appChainIds: [84532],
+      });
+      const provider = sdk.makeWeb3Provider();
+      const address = await provider.request({
+        method: 'eth_requestAccounts',
+      });
+      console.log(address)
+      setConnected(true)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const disconnect = async () => {
+    try {
+      setConnected(false)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="app logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>app</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <PWABadge />
-    </>
+    <div className='app'>
+      <h1>Smart Wallet</h1>
+      {!connected && <button onClick={connect}>connect</button>}
+      {connected && <button onClick={disconnect}>disconnect</button>}
+    </div>
   )
 }
 
